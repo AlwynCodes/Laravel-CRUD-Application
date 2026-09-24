74797ce637184cf55a0903e26f90f459fa03be8b
AlwynCodes <alwynjade.reuyan@g.msuiit.edu.ph>
2026-09-24 20:02:08 +0800

    fix: resolve duplicate index method in ProductController and add create route

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -10,4 +10,8 @@ class ProductController extends Controller
     {
         return view('products.index');
     }
+       public function index()
+    {
+        return view('products.create');
+    }
 }
