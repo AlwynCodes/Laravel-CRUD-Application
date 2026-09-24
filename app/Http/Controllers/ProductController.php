cbcac4294c0f22c58e53cb600602484da7ed9349
AlwynCodes <alwynjade.reuyan@g.msuiit.edu.ph>
2026-09-24 20:52:11 +0800

    fix: add missing products variable to index view return statement

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -9,6 +9,7 @@ class ProductController extends Controller
 {
     public function index(){
         return view('products.index');
+        return view('products.index', ['products' => $products]);
     }
 
     public function create(){
