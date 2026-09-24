77fe709e1233d2420dea06f0a47ee34c6ec00b71
AlwynCodes <alwynjade.reuyan@g.msuiit.edu.ph>
2026-09-24 20:57:01 +0800

    fix: correct product header and improve table structure in index view

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -6,7 +6,32 @@
     <title>Document</title>
 </head>
 <body>
-    <h1>Products</h1>
+    <h1>Product</h1>
     <div>index</div>
+    <table border="1">
+        <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Qty</th>
+            <th>Price</th>
+            <th>Description</th>
+        </tr>
+        @foreach($products as $product)
+            <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Qty</th>
+                <th>Price</th>
+                <th>Description</th>
+            </tr>
+            <tr>
+                <td>{{ $product->id }}</td>
+                <td>{{ $product->name }}</td>
+                <td>{{ $product->qty }}</td>
+                <td>{{ $product->price }}</td>
+                <td>{{ $product->description }}</td>
+            </tr>
+        @endforeach
+    </table> 
 </body>
 </html>
