a370cf4d8b8e901d28eb92595a8e1ae2aeba1bee
AlwynCodes <alwynjade.reuyan@g.msuiit.edu.ph>
2026-09-24 20:08:21 +0800

    fix: update create product view with form fields and structure

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -5,7 +5,30 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-<body>
-    <h1>Create Product</h1>
-</body>
+</head>
+    <body>
+        <h1>Create a Product</h1>
+        <form method="post" action="">
+            <div>
+                <label>Name</label>
+            </div>
+            <div>
+                <div>
+                    <input type="text" name="name" placeholder="Name"/>
+                </div>
+                <div>
+                    <input type="text" name="qty" placeholder="qty"/>
+                </div>
+                <div>
+                    <input type="text" name="price" placeholder="price"/>
+                </div>
+                <div>
+                    <input type="text" name="description" placeholder="description"/>
+                </div>
+                <div>
+                    <input type="submit" value="Save a new Product"/>
+                </div>
+            </div>
+        </form>
+    </body>
 </html>
